709d4bb69656cdce6988df44b3076f0f994b78b0
특이점이 온다 <114588231+m161awm2@users.noreply.github.com>
2026-03-10 20:17:33 +0900

    Update serverHelper.py

--- serverHelper.py
+++ serverHelper.py
@@ -2,9 +2,9 @@ import subprocess
 print("서버 도우미가 실행됩니다.")
 ACCESS_LOG = "/var/log/nginx/access.log"
 ERROR_LOG = "/var/log/nginx/error.log"
-print("======= 선 택 =======")
-print("1. 접속 로그 확인\n2.오류 로그 확인\n3.nginx 상태 확인\n4.종료")
 while 1:
+    print("======= 선 택 =======")
+    print("1. 접속 로그 확인\n2.오류 로그 확인\n3.nginx 상태 확인\n4.종료")
     choice = int(input())
     if choice == 1:
         with open(ACCESS_LOG, "r") as f:
@@ -20,3 +20,4 @@ while 1:
         break
     else:
         print("?")
+
